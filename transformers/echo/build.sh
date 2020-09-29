fe36688334f1fe5f2437e78f137fb4c6cd0eab7a
Janusz Marcinkiewicz <januszm@nvidia.com>
2020-09-29 11:43:43 +0200

    Replace bash scripts with Makefiles

--- transformers/echo/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -t ${DOCKER_REGISTRY_URL:-"localhost:5000"}/transformer_echo:latest .
--- transformers/hello_world/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -t ${DOCKER_REGISTRY_URL:-"localhost:5000"}/transformer_hello_world:latest .
