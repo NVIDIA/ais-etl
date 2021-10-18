bdf630fed745bbb3f98c191ae4466c661bac306d
Janusz Marcinkiewicz <januszm@nvidia.com>
2021-10-18 09:57:35 +0200

    package: move `cloudpickle` package from extras require to install require
    Signed-off-by: Janusz Marcinkiewicz <januszm@nvidia.com>

--- package/setup.py
+++ package/setup.py
@@ -52,10 +52,9 @@ setup(
     license="MIT",
     python_requires='>=3.6',
     packages=find_packages(exclude=("tests", "tests.*")),
-    install_requires=['requests'],
+    install_requires=['requests', 'cloudpickle==2.0.0'],
     extras_require={
         'pytorch': ['torch', 'torchvision'],
         'tf': ['braceexpand', 'humanfriendly', 'tensorflow', 'tensorflow_addons'],
-        'cloudpickle': ['cloudpickle==2.0.0'],
     },
 )
