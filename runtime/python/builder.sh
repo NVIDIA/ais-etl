e60e4c5a8be208379916fc245fd874f670336ce2
Janusz Marcinkiewicz <januszm@nvidia.com>
2021-10-18 11:00:26 +0200

    runtime: chores in builder script
    Signed-off-by: Janusz Marcinkiewicz <januszm@nvidia.com>

--- runtime/python/builder.sh
+++ runtime/python/builder.sh
@@ -2,18 +2,19 @@
 
 set -e
 
+# Defines mapping: "runtime name" -> "python version".
 declare -A python_versions=(
   [2]="2.7.18"
   [3]="3.8.5"
   [3.6]="3.6"
   [3.8]="3.8"
   [3.10]="3.10"
-);
+)
 
-for version_name in "${!python_versions[@]}"; do
+for runtime_name in "${!python_versions[@]}"; do
 	docker build --pull --no-cache \
-	  -t "${REGISTRY_URL}/aistore/runtime_python:${version_name}${RUNTIME_TAG_MODIFIER}" \
-	  --build-arg PYTHON_VERSION="${python_versions[${version_name}]}" \
+	  -t "${REGISTRY_URL}/aistore/runtime_python:${runtime_name}${RUNTIME_TAG_MODIFIER}" \
+	  --build-arg PYTHON_VERSION="${python_versions[${runtime_name}]}" \
 	  .
-	docker push "${REGISTRY_URL}/aistore/runtime_python:${version_name}${RUNTIME_TAG_MODIFIER}"
+	docker push "${REGISTRY_URL}/aistore/runtime_python:${runtime_name}${RUNTIME_TAG_MODIFIER}"
 done
