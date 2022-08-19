b23b3c85b7c571b9f24d62a080a56e5f892e2d05
Abhishek Gaikwad <gaikwadabhishek1997@gmail.com>
2022-08-19 12:29:54 -0700

    added filter and updated stream logic
    Signed-off-by: Abhishek Gaikwad <gaikwadabhishek1997@gmail.com>

--- runtime/python/server.py
+++ runtime/python/server.py
@@ -17,6 +17,8 @@ host_target = os.environ["AIS_TARGET_URL"]
 code_file = os.getenv("MOD_NAME")
 mod = imp.load_source("function", f"./code/{code_file}.py")
 
+FIRST_CHUNK_SIZE_FILTER = 8192
+
 try:
     CHUNK_SIZE = int(os.getenv("CHUNK_SIZE"))
 except Exception:
@@ -39,6 +41,7 @@ try:
     FILTER_EXISTS = True
 except Exception:
     FILTER_EXISTS = False
+
 
 transform = getattr(mod, os.getenv("FUNC_TRANSFORM"))
 
@@ -69,10 +72,26 @@ class Handler(BaseHTTPRequestHandler):
     def do_PUT(self):
 
         content_length = int(self.headers["Content-Length"])
+        object_name = self.path
 
-        # TODO: handle filters
-        # if FILTER_EXISTS and not filter(self.headers["Name"], content_length):
-        # self._set_headers()
+        if FILTER_EXISTS:
+            # retreive first chunk
+            first_chunk_size = FIRST_CHUNK_SIZE_FILTER
+
+            if CHUNK_SIZE > 0:
+                first_chunk_size = CHUNK_SIZE
+
+            first_chunk_size = min(first_chunk_size, content_length)
+            first_chunk = self.rfile.read(first_chunk_size)
+
+            if not filter(object_name, first_chunk):
+                self.send_response_only(204)  # or 200
+                self.end_headers()
+                return
+
+            content_length -= first_chunk_size
+        else:
+            first_chunk = b""
 
         # populate context
         context = {}
@@ -80,31 +99,52 @@ class Handler(BaseHTTPRequestHandler):
         if BEFORE_EXISTS:
             before(context)
 
+        self._set_headers()
         if CHUNK_SIZE == 0:
-            params = len(signature(transform).parameters)
-            context["result"] = (
-                transform(self.rfile.read(content_length), context)
-                if params > 1
-                else transform(self.rfile.read(content_length))
+            print("here")
+            input_bytes = (
+                first_chunk
+                if content_length == 0
+                else (first_chunk + self.rfile.read(content_length))
             )
+            print("ip", input_bytes[:5])
+            context["result"] = (
+                transform(input_bytes, context)
+                if len(signature(transform).parameters) > 1
+                else transform(input_bytes)
+            )
+            print("op", context["result"][:5])
         else:
             context["transformed-length"] = 0
             context["remaining-length"] = content_length
+
+            if FILTER_EXISTS and len(first_chunk) != 0:
+                output = transform(first_chunk, context)
+                if output is not None:
+                    self.wfile.write(output)
+                context["transformed-length"] += first_chunk_size
+
             while context["remaining-length"] > 0:
-                read_buffer = (
+                read_buffer_size = (
                     CHUNK_SIZE
                     if context["remaining-length"] >= CHUNK_SIZE
                     else context["remaining-length"]
                 )
-                # user expected to store partial result in context
-                transform(self.rfile.read(read_buffer), context)
-                context["transformed-length"] += read_buffer
-                context["remaining-length"] -= read_buffer
+
+                # if transform functions returns anything return it back to user
+                output = transform(self.rfile.read(read_buffer_size), context)
+
+                if output is not None:
+                    self.wfile.write(output)
+
+                context["transformed-length"] += read_buffer_size
+                context["remaining-length"] -= read_buffer_size
 
         if AFTER_EXISTS:
             context["result"] = after(context)
-        self._set_headers()
-        self.wfile.write(context["result"])
+
+        if "result" in context and context["result"] != None:
+            self.wfile.write(context["result"])
 
     def do_GET(self):
         if self.path == "/health":
