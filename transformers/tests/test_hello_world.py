532e23686ffb04bf2dd73a3f9c3f0810d0dc474c
Ryan Koo <rbk65@cornell.edu>
2023-08-01 00:18:13 -0700

    transformers: add hpush and hrev tests for hello world
    Signed-off-by: Ryan Koo <rbk65@cornell.edu>

--- transformers/tests/test_hello_world.py
+++ transformers/tests/test_hello_world.py
@@ -6,11 +6,11 @@
 import os
 import unittest
 
+from aistore.sdk.etl_const import ETL_COMM_HPULL, ETL_COMM_HPUSH, ETL_COMM_HREV
+from aistore.sdk.etl_templates import HELLO_WORLD
+
 from tests.test_base import TestBase
 from tests.utils import git_test_mode_format_image_tag_test
-
-from aistore.sdk.etl_const import ETL_COMM_HPULL
-from aistore.sdk.etl_templates import HELLO_WORLD
 
 
 class TestHelloWorldTransformer(TestBase):
@@ -23,28 +23,39 @@ class TestHelloWorldTransformer(TestBase):
         self.test_bck.object(self.test_image_filename).put_file(self.test_image_source)
         self.test_bck.object(self.test_text_filename).put_file(self.test_text_source)
 
-    @unittest.skipIf(
-        os.getenv("HELLO_WORLD_ENABLE", "true") == "false", "HELLO_WORLD is disabled"
-    )
-    def test_hello_world(self):
-        template = HELLO_WORLD.format(communication_type=ETL_COMM_HPULL)
+    def compare_transformed_data_with_hello_world(self, filename: str):
+        transformed_data_bytes = (
+            self.test_bck.object(filename).get(etl_name=self.test_etl.name).read_all()
+        )
+        self.assertEqual(b"Hello World!", transformed_data_bytes)
+
+    def run_hello_world_test(self, communication_type: str):
+        template = HELLO_WORLD.format(communication_type=communication_type)
 
         if self.git_test_mode == "true":
             template = git_test_mode_format_image_tag_test(template, "hello_world")
 
-        self.test_etl.init_spec(template=template, communication_type=ETL_COMM_HPULL)
-
-        transformed_image_bytes = (
-            self.test_bck.object(self.test_image_filename)
-            .get(etl_name=self.test_etl.name)
-            .read_all()
-        )
-        transformed_text_bytes = (
-            self.test_bck.object(self.test_text_filename)
-            .get(etl_name=self.test_etl.name)
-            .read_all()
+        self.test_etl.init_spec(
+            template=template, communication_type=communication_type
         )
 
-        # Compare file contents
-        self.assertEqual(b"Hello World!", transformed_image_bytes)
-        self.assertEqual(b"Hello World!", transformed_text_bytes)
+        self.compare_transformed_data_with_hello_world(self.test_image_filename)
+        self.compare_transformed_data_with_hello_world(self.test_text_filename)
+
+    @unittest.skipIf(
+        os.getenv("HELLO_WORLD_ENABLE", "true") == "false", "HELLO_WORLD is disabled"
+    )
+    def test_hello_world_hpull(self):
+        self.run_hello_world_test(ETL_COMM_HPULL)
+
+    @unittest.skipIf(
+        os.getenv("HELLO_WORLD_ENABLE", "true") == "false", "HELLO_WORLD is disabled"
+    )
+    def test_hello_world_hpush(self):
+        self.run_hello_world_test(ETL_COMM_HPUSH)
+
+    @unittest.skipIf(
+        os.getenv("HELLO_WORLD_ENABLE", "true") == "false", "HELLO_WORLD is disabled"
+    )
+    def test_hello_world_hrev(self):
+        self.run_hello_world_test(ETL_COMM_HREV)
