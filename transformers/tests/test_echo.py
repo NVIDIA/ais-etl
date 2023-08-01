95b73aa7218dd26ac40915b4cdd653d478726060
Ryan Koo <rbk65@cornell.edu>
2023-07-31 23:21:32 -0700

    transformers: add hrev and hpush tests for echo
    Signed-off-by: Ryan Koo <rbk65@cornell.edu>

--- transformers/tests/test_echo.py
+++ transformers/tests/test_echo.py
@@ -1,16 +1,17 @@
 #
 # Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
 #
+
 # pylint: disable=missing-class-docstring, missing-function-docstring, missing-module-docstring
 
 import os
 import unittest
 
+from aistore.sdk.etl_const import ETL_COMM_HPULL, ETL_COMM_HPUSH, ETL_COMM_HREV
+from aistore.sdk.etl_templates import ECHO
+
 from tests.test_base import TestBase
 from tests.utils import git_test_mode_format_image_tag_test
-
-from aistore.sdk.etl_const import ETL_COMM_HPULL
-from aistore.sdk.etl_templates import ECHO
 
 
 class TestEchoTransformer(TestBase):
@@ -23,32 +24,40 @@ class TestEchoTransformer(TestBase):
         self.test_bck.object(self.test_image_filename).put_file(self.test_image_source)
         self.test_bck.object(self.test_text_filename).put_file(self.test_text_source)
 
-    @unittest.skipIf(os.getenv("ECHO_ENABLE", "true") == "false", "ECHO is disabled")
-    def test_echo(self):
-        template = ECHO.format(communication_type=ETL_COMM_HPULL)
+    def initialize_template(self, communication_type: str):
+        template = ECHO.format(communication_type=communication_type)
 
         if self.git_test_mode == "true":
             template = git_test_mode_format_image_tag_test(template, "echo")
 
-        self.test_etl.init_spec(template=template, communication_type=ETL_COMM_HPULL)
-
-        transformed_image_bytes = (
-            self.test_bck.object(self.test_image_filename)
-            .get(etl_name=self.test_etl.name)
-            .read_all()
-        )
-        transformed_text_bytes = (
-            self.test_bck.object(self.test_text_filename)
-            .get(etl_name=self.test_etl.name)
-            .read_all()
+        self.test_etl.init_spec(
+            template=template, communication_type=communication_type
         )
 
-        # Compare image content
-        with open(self.test_image_source, "rb") as file:
-            original_image_content = file.read()
-        self.assertEqual(transformed_image_bytes, original_image_content)
+    def compare_transformed_data(self, filename: str, source: str):
+        transformed_bytes = (
+            self.test_bck.object(filename).get(etl_name=self.test_etl.name).read_all()
+        )
 
-        # Compare text content
-        with open(self.test_text_source, "r", encoding="utf-8") as file:
-            original_text_content = file.read()
-        self.assertEqual(transformed_text_bytes.decode("utf-8"), original_text_content)
+        with open(source, "rb") as file:
+            original_content = file.read()
+
+        self.assertEqual(transformed_bytes, original_content)
+
+    @unittest.skipIf(os.getenv("ECHO_ENABLE", "true") == "false", "ECHO is disabled")
+    def test_echo_hpull(self):
+        self.initialize_template(ETL_COMM_HPULL)
+        self.compare_transformed_data(self.test_image_filename, self.test_image_source)
+        self.compare_transformed_data(self.test_text_filename, self.test_text_source)
+
+    @unittest.skipIf(os.getenv("ECHO_ENABLE", "true") == "false", "ECHO is disabled")
+    def test_echo_hpush(self):
+        self.initialize_template(ETL_COMM_HPUSH)
+        self.compare_transformed_data(self.test_image_filename, self.test_image_source)
+        self.compare_transformed_data(self.test_text_filename, self.test_text_source)
+
+    @unittest.skipIf(os.getenv("ECHO_ENABLE", "true") == "false", "ECHO is disabled")
+    def test_echo_hrev(self):
+        self.initialize_template(ETL_COMM_HREV)
+        self.compare_transformed_data(self.test_image_filename, self.test_image_source)
+        self.compare_transformed_data(self.test_text_filename, self.test_text_source)
