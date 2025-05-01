6d50d26afededb1b93265158821dc5b37c00f2c7
Tony Chen <a122774007@gmail.com>
2025-05-01 10:44:07 -0700

    transformer: update Go-based ETLs to use generic webserver framework
    Signed-off-by: Tony Chen <a122774007@gmail.com>

--- transformers/go_hello_world/src/main.go
+++ transformers/go_hello_world/src/main.go
@@ -5,123 +5,36 @@
 package main
 
 import (
+	"bytes"
 	"flag"
-	"fmt"
 	"io"
 	"log"
-	"net/http"
-	"os"
-	"strings"
+
+	"github.com/NVIDIA/aistore/ext/etl/webserver"
 )
 
-var (
-	aisTargetURL string
-	endpoint     string
-
-	client *http.Client
-	logger *log.Logger
-)
-
-func initVars(ipAddress string, port int) {
-	endpoint = fmt.Sprintf("%s:%d", ipAddress, port)
-	aisTargetURL = os.Getenv("AIS_TARGET_URL")
-	client = &http.Client{}
+type HelloWorldServer struct {
+	response string
+	webserver.ETLServer
 }
+
+func (es *HelloWorldServer) Transform(input io.ReadCloser, path, args string) (io.ReadCloser, error) {
+	input.Close()
+	return io.NopCloser(bytes.NewReader([]byte(es.response))), nil
+}
+
+var _ webserver.ETLServer = (*HelloWorldServer)(nil)
 
 func main() {
-	var (
-		ipAddressArg = flag.String("l", "localhost", "Specify the IP address on which the server listens")
-		portArg      = flag.Int("p", 8000, "Specify the port on which the server listens")
-	)
-
+	listenAddr := flag.String("l", "0.0.0.0", "IP address to listen on")
+	port := flag.Int("p", 80, "Port to listen on")
 	flag.Parse()
 
-	initVars(*ipAddressArg, *portArg)
-
-	logger = log.New(os.Stdout, "[HelloWorld] ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
-
-	http.HandleFunc("/", helloWorldHandler)
-	http.HandleFunc("/health", healthHandler)
-
-	logger.Printf("Starting hello world transformer at %s", endpoint)
-	logger.Fatal(http.ListenAndServe(endpoint, nil))
-}
-
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, fmt.Sprintf("Invalid HTTP method %q", r.Method), http.StatusBadRequest)
-		return
-	}
-	if _, err := w.Write([]byte("Running")); err != nil {
-		logError(err, fmt.Sprintf("Error writing response for %q", r.URL.EscapedPath()))
-	}
-}
-
-func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodPut:
-		putHandler(w, r)
-	case http.MethodGet:
-		geHandler(w, r)
-	default:
-		http.Error(w, fmt.Sprintf("Invalid HTTP method %q, expected %q or %q", r.Method, http.MethodPut, http.MethodGet), http.StatusBadRequest)
-	}
-}
-
-// PUT /
-func putHandler(w http.ResponseWriter, r *http.Request) {
-	escapePath := r.URL.EscapedPath()
-
-	defer r.Body.Close()
-	readContent(w, r.Body, r.ContentLength, escapePath)
-	writeHelloWorld(w, escapePath)
-}
-
-// GET /
-func geHandler(w http.ResponseWriter, r *http.Request) {
-	if aisTargetURL == "" {
-		http.Error(w, "Missing AIS_TARGET_URL environment variable", http.StatusBadRequest)
-		return
+	svr := &HelloWorldServer{
+		response: "Hello World!",
 	}
 
-	escapePath := r.URL.EscapedPath()
-	path := strings.TrimPrefix(escapePath, "/")
-
-	resp, err := client.Get(aisTargetURL + "/" + path)
-	if err != nil {
-		logAndRespondError(w, err, fmt.Sprintf("GET to AIStore failed for  %q", escapePath), http.StatusInternalServerError)
-		return
-	}
-	defer resp.Body.Close()
-	readContent(w, resp.Body, r.ContentLength, r.URL.EscapedPath())
-	writeHelloWorld(w, escapePath)
-
-}
-
-func logAndRespondError(w http.ResponseWriter, err error, msg string, status int) {
-	logError(err, msg)
-	http.Error(w, msg, status)
-}
-
-func logError(err error, msg string) {
-	logger.Printf("%s: %v\n", msg, err)
-}
-
-func readContent(w http.ResponseWriter, body io.ReadCloser, contentLength int64, path string) {
-	n, err := io.Copy(io.Discard, body)
-
-	if err != nil {
-		logAndRespondError(w, err, fmt.Sprintf("Error reading request body for %q", path), http.StatusBadRequest)
-		return
-	}
-	if contentLength > 0 && contentLength != int64(n) {
-		logAndRespondError(w, nil, fmt.Sprintf("Content length mismatch for %q", path), http.StatusBadRequest)
-		return
-	}
-}
-
-func writeHelloWorld(w http.ResponseWriter, path string) {
-	if _, err := w.Write([]byte("Hello World!")); err != nil {
-		logError(err, fmt.Sprintf("Error writing response for %q", path))
+	if err := webserver.Run(svr, *listenAddr, *port); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
 	}
 }
